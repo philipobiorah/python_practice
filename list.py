cfd1877eb3c0ea27a3a72d00753bc7290e735b4e
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-02-27 21:27:08 +0000

    removing element from list using del

--- list.py
+++ list.py
@@ -12,6 +12,12 @@ bicycles.append("awesome")
 
 
 #inserting items into the list
+bicycles.insert(0, "brilliant")
+bicycles.insert(3, "splendid")
+
+
+#removing element form list using del
+del bicycles[3]
 
 for i in range(len(bicycles)-1):
     print(bicycles[i].title())
