87b9ea97d74275529331d683755ec797dc7e31a3
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-02-27 22:33:16 +0000

    print current guestlist

--- guestlist.py
+++ guestlist.py
@@ -1,5 +1,20 @@
-guestlist = ['chigozie', 'tii', 'uncle inno', 'semper']
+guestlist = ['chigozie', 'tii', 'samba', 'uncle inno', 'semper' ]
 
 
 for i in range(len(guestlist)-1):
     print(f"{guestlist[i].title()} you are invited to my Graduation Ceremoney")
+
+
+cannot_make_it = ['semper','samba']
+
+
+
+for p in cannot_make_it:
+    guestlist.remove(p)
+
+
+guestlist.insert(1, "Atimor")
+
+print("Current List:")
+for guest in guestlist:
+    print(guest.title())
