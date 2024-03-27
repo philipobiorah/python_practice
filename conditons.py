9edcac95baec7540ab13e5bac3441e98aff66d90
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-03-27 23:31:29 +0000

    modified conditions.py

--- conditons.py
+++ conditons.py
@@ -10,3 +10,18 @@ if 'extra cheese' in requested_toppings:
     print("Adding extra cheese. ")
 
 print("\nFinshed making your pizza!")
+
+
+
+
+
+#checking if the list is empty
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}. ")
+    print("\nFinished making your pizza!")
+
+else:
+    print("Are you sure you want a plain pizza?")
