ed2bdff316384c1f67fe568045be27cdd953c3fe
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-04-12 23:41:26 +0100

    completed ShowCurrentTime.py

--- ShowCurrentTime.py
+++ ShowCurrentTime.py
@@ -7,5 +7,26 @@ print(f" total {totalSeconds}")
 
 #Get the current second
 currentSecond = totalSeconds% 60
-print(currentSecond)
+print(f"currentSecond: {currentSecond}")
 
+#Obtain the total minutes
+totalMinutes = totalSeconds // 60
+print(f"totalMiuntes: {totalMinutes}")
+
+#obtain current minute 
+currentMinute =  totalMinutes %  60
+print(f"Current Minute: {currentMinute}")
+
+#Obtain the total number of hours
+totalHours = totalMinutes // 60
+print(f"Total Hours: {totalHours}")
+
+#Obtain the current hour
+currenthour =  totalHours % 24
+print(f"Total Hours: {currenthour}")
+
+
+
+#Display resutls 
+print("Current time is", currenthour, ":",
+      currentMinute, ":", currentSecond, "GMT")
