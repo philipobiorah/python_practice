e201ed5ee052dbdee00b5f05e775ada40aec7216
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-04-13 23:00:28 +0100

    added computeDistance.py

---
+++ computeDistance.py
@@ -0,0 +1,7 @@
+# Enter the first point with two double values
+
+x1 = float(input("Enter x-coordinate for Point 1: "))
+y1 = float(input("Enter y-coordinate for Point 1: "))
+
+
+# Enter the second poin
