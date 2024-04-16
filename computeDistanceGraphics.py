27f2bdbc2f3d8fed749c9b63c003b3f08d3edef0
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-04-16 23:48:33 +0100

    added ComputeDistance Graphics

---
+++ computeDistanceGraphics.py
@@ -0,0 +1,8 @@
+import turtle
+
+#Prompt the user for inputing two points
+
+x1 = float(input("Enter x-coordinate for  Point 1: "))
+y1 = float(input("Enter y-coordinate for Point 2:  "))
+x2 = float(input("enter x-coordinate for Point 2: "))
+y2 = float(input("Enter y-coordinate for Point 2: "))
