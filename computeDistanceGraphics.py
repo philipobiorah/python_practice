19783bd8299a2b45acc0b61f9044b8672714ba1e
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-04-17 22:51:01 +0100

    Compute the distance

--- computeDistanceGraphics.py
+++ computeDistanceGraphics.py
@@ -6,3 +6,9 @@ x1 = float(input("Enter x-coordinate for  Point 1: "))
 y1 = float(input("Enter y-coordinate for Point 2:  "))
 x2 = float(input("enter x-coordinate for Point 2: "))
 y2 = float(input("Enter y-coordinate for Point 2: "))
+
+
+#Compute the distance
+distance = ((x2 - x1)**2 +(y2 - y1)**2)**5
+
+#Display two oints and the connecting line
