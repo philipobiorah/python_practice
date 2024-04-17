1fc90f3b3670205c150c73f044eac77994ad504f
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-04-17 23:33:08 +0100

    completed computedistancegraphics

--- computeDistanceGraphics.py
+++ computeDistanceGraphics.py
@@ -9,10 +9,16 @@ y2 = float(input("Enter y-coordinate for Point 2: "))
 
 
 #Compute the distance
-distance = ((x2 - x1)**2 +(y2 - y1)**2)**5
+distance = ((x2 - x1)**2 +(y2 - y1)**2)**0.5
 
 #Display two points and the connecting line
 turtle.penup()
 turtle.goto(x1, y1)  # Move to (x1, y1)
 turtle.pendown()
 turtle.write("Point 1", font=("Times", 12))
+turtle.goto(x2, y2) # Draw a line to  (x2, y2)
+turtle.write("Point 2", font=("Times", 12))
+turtle.penup()
+turtle.goto((x2  + x1)/2 , (y2 + y1)/2)
+turtle.write(distance, font=("Times", 12))
+turtle.done()
