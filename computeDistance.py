43a456f5f377f915a3048b29cafc744a45113aa1
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-04-24 22:44:03 +0100

    using computedistance fuction

--- computeDistance.py
+++ computeDistance.py
@@ -15,6 +15,13 @@ x2 = float(input("Enter x-coordinate for Point 2: "))
 y2 = float(input("Enter y-coordinate for Point 2: "))
 
 
-# Compute the distance
-distance = ((x2 - x1)**2 + (y2 - y1) **2)** 0.5
-print("The distance between the two point is ", distance)
+def computedistance(x1,y1, x2, y2):
+
+
+    # Compute the distance
+    distance = ((x2 - x1)**2 + (y2 - y1) **2)** 0.5
+    print("The distance between the two point is ", distance)
+
+
+
+computedistance(x1,y1, x2, y2)
