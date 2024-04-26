07897690510cdd6780138190f7a559d40004ea3e
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-04-26 23:46:03 +0100

    added calculateArea of Triangle.py

---
+++ exercise_2/calculateArea_of_triangle.py
@@ -0,0 +1,11 @@
+
+
+
+
+def computedistance(x1,y1, x2, y2):
+
+    # Compute the distance
+    distance = ((x2 - x1)**2 + (y2 - y1) **2)** 0.5
+    print("The distance between the two point is ", distance)
+
+computedistance(x1,y1, x2, y2)
