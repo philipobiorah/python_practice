ca0bca311d6eafc3655893c64944cc86c79b23de
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-06-28 21:32:19 +0100

    added compound interest.py

---
+++ compound_intrest.py
@@ -0,0 +1,14 @@
+"""
+A person invest $1000 in a savings account yielding 5% interest. Assumming tht the person leaves all interst on depoist in the accont, calculate 
+and display the amount of money in the account at the end of each year for 10 yars. formular ''   a = p(1 + r)**n
+
+
+"""
+
+principal = 1000
+rate = 5/100
+
+for year in range(1, 11):
+    amount = principal * (1 + rate) ** year
+    print(f'{year:>2} {amount:.2f}')
+  
