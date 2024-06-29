bb6de60b71e82c48720a5d8cff65bbc1273e3b5f
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-06-29 23:43:13 +0100

    updated random_gen

--- random_number_gen.py
+++ random_number_gen.py
@@ -1,4 +1,12 @@
 import random 
 
+
+#random.seed(32)
 for roll in range(10):
-    print(random.randrange(1, 7), end= ' ')
+    print(random.randrange(1,7), end= ' ')
+
+print()
+
+
+for i in range(20):
+    print('H' if random.randrange(2) == 0 else 'T', end= ' ')
