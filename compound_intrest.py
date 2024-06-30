722646c6c33809526e6a7bb93678e4140b2f36b8
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-06-30 23:50:28 +0100

    modified compound_intrest.py

--- compound_intrest.py
+++ compound_intrest.py
@@ -9,6 +9,12 @@ principal = 1000
 rate = 5/100
 
 for year in range(1, 11):
+    if year == 5:
+        continue
     amount = principal * (1 + rate) ** year
-    print(f'{year:>2} {amount:.2f}')
+    print(f'{year:>2} {amount:>10.2f}')
   
+
+
+
+
